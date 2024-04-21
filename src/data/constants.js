ddf1ef5851e28383978b13e99fac044f0da84e9b
Ridhwiman Roy <141118351+R1dhwiman@users.noreply.github.com>
2024-04-21 15:54:03 +0530

    Update constants.js
    Updated resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -12,7 +12,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/R1dhwiman",
   resume:
-    "https://drive.google.com/file/d/1eV04fxSfuYMkRe48f8ML2bLyUgObZ2Ry/view?usp=sharing",
+    "https://drive.google.com/file/d/1UthHYEasWnaG59_8yAbHYaM2pBjez660/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/ridhwimanroy/",
   insta: "https://www.instagram.com/ridhwiman_roy/",
   facebook: "https://www.facebook.com/ridhwiman.roy?mibextid=LQQJ4d/",
