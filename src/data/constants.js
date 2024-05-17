a15abdad5eb44b2b4ea1390b3ebc6c112d32d3ac
Ridhwiman <rikroy2399@gmail.com>
2024-05-17 14:35:56 +0530

    resume updated

--- src/data/constants.js
+++ src/data/constants.js
@@ -12,7 +12,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/R1dhwiman",
   resume:
-    "https://drive.google.com/file/d/1P-UgZsxkSusMO-ehhd65GJenanU2ReLb/view?usp=sharing",
+    "https://drive.google.com/file/d/1Tu6tCP7ypo4Ts-iHt9yFN30zbnCo62_K/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/ridhwimanroy/",
   insta: "https://www.instagram.com/ridhwiman_roy/",
   facebook: "https://www.facebook.com/ridhwiman.roy?mibextid=LQQJ4d/",
